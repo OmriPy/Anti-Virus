845d9dd65b0d66dfeb923e7d7e01e4aae4762239
Omri <110406612+OmriPy@users.noreply.github.com>
2024-03-24 11:41:18 +0200

    Update server.py

--- source_code/VMs_Side/server.py
+++ source_code/VMs_Side/server.py
@@ -2,25 +2,28 @@ from protocol import *
 from threading import Thread
 
 def handle_client(client: socket):
-    print('A client has connected')
-    '''try:
+    print_colored('server', 'Client has connected')
+    try:
         send(client, 'This is a message from the server!')
     except ProtocolError as e:
         print(e)
-        return'''
+        return
+
+def handle_anti_virus(anti_virus: socket):
+    print_colored('server', 'Anti Virus has connected')
     while True:
-        print(recv(client))
-        send(client, Messages.OK)    # for now, doesn't check the value of the message, if it is alright or not
+        print_colored('anti virus', recv(anti_virus))
+        send(anti_virus, Messages.OK)    # for now, doesn't check the value of the message, if it is alright or not
     client.close()
 
 def server():
     with listening_socket('0.0.0.0') as server:
-        print()
         while True:
             try:
                 client, cli_add = server.accept()
             except KeyboardInterrupt:
                 return
+            identity = recv(client)
             thrd = Thread(target=handle_client, args=(client,))
             thrd.start()
 
