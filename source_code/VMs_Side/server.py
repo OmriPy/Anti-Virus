0fb9606595dfdd14c00972a8ea9b95a6bce083c7
Omri <110406612+OmriPy@users.noreply.github.com>
2024-03-24 13:01:55 +0200

    Update server.py

--- source_code/VMs_Side/server.py
+++ source_code/VMs_Side/server.py
@@ -2,29 +2,49 @@ from protocol import *
 from threading import Thread
 
 def handle_client(client: socket):
-    print_colored('server', 'Client has connected')
+    print_colored('server', Messages.CLIENT_CONNECTED)
     try:
         send(client, 'This is a message from the server!')
     except ProtocolError as e:
-        print(e)
+        print_colored('error', e)
         return
 
 def handle_anti_virus(anti_virus: socket):
-    print_colored('server', 'Anti Virus has connected')
+    print_colored('server', Messages.ANTI_VIRUS_CONNECTED)
     while True:
-        print_colored('anti virus', recv(anti_virus))
-        send(anti_virus, Messages.OK)    # for now, doesn't check the value of the message, if it is alright or not
-    client.close()
+        anti_virus_msg = recv(anti_virus)
+        print_colored('anti virus', anti_virus_msg)
+        send(anti_virus, Messages.OK)
+        if anti_virus_msg == Messages.CONNECTION_CLOSED:
+            break
+    anti_virus.close()
+
+clients: Dict[str, Callable] = {
+    Messages.ANTI_VIRUS_CONNECTED: handle_anti_virus,
+    Messages.CLIENT_CONNECTED: handle_client
+}
 
 def server():
     with listening_socket('0.0.0.0') as server:
         while True:
             try:
                 client, cli_add = server.accept()
+                identity_msg = recv(client)
             except KeyboardInterrupt:
+                print_colored('info', Messages.CTRL_C)
+                client.close()
                 return
-            identity = recv(client)
-            thrd = Thread(target=handle_client, args=(client,))
+            except ProtocolError as e:
+                print_colored('error', e)
+                client.close()
+                return
+            if identity_msg in clients:
+                thrd = Thread(target=clients[identity_msg], args=(client,))
+            else:
+                print_colored('error', 'Unknown socket has connected! Neither client nor Anti Virus')
+                client.close()
+                return
+            send(client, Messages.OK)
             thrd.start()
 
 
