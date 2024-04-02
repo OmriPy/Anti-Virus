a0dd62dc314ccf1ce4c670801ff8d64c47d16005
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-02 15:40:08 +0300

    Update clientUtils.py

--- source_code/VMs_Side/clientUtils.py
+++ source_code/VMs_Side/clientUtils.py
@@ -1,35 +1,21 @@
 from protocol import *
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFrame, QLabel, QListView
-from PyQt6.QtCore import QStringListModel
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFrame, QLabel, QListWidget
 
 class Page(QFrame):
 
     def __init__(self):
         super().__init__()
 
-        self.associated_widgets: List[QWidget] = []
-
-    def associate_widget(self, widg: QWidget):
-        self.associated_widgets.append(widg)
-
     def remove(self):
-        for widg in self.associated_widgets:
-            widg.setParent(None)
         self.setParent(None)
 
 
-class ItemsList(QListView):
+class ItemsList(QListWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.data_model = QStringListModel()
-        self.data: List[str] = []
-    
-    def setData(self, data: List[str]):
-        self.data = data
-        self.data_model.setStringList(data)
+        
 
-    def addData(self, data: List[str]):
-        self.data.extend(data)
-        self.data_model.setStringList(data)
+    def addData(self, data: str):
+        pass
