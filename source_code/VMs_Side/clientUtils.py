9d97a900a8a2d130a658f1b8d2e52373d4a3c86c
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-02 18:20:39 +0300

    Update clientUtils.py

--- source_code/VMs_Side/clientUtils.py
+++ source_code/VMs_Side/clientUtils.py
@@ -1,10 +1,21 @@
 from protocol import *
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFrame, QLabel, QListWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFrame, QLabel, QListWidget, QListWidgetItem
 
 class Page(QFrame):
 
-    def __init__(self):
+    def __init__(self, width: int = 0, height: int = 0):
         super().__init__()
+
+        # Create layout for frame
+        self.frame_layout = QVBoxLayout()
+        self.setLayout(self.frame_layout)
+
+        # Change size if arguments were given
+        if width > 0 and height > 0:
+            self.setFixedSize(width, height)
+
+    def addWidget(self, widg: QWidget):
+        self.frame_layout.addWidget(widg)
 
     def remove(self):
         self.setParent(None)
@@ -15,7 +26,8 @@ class ItemsList(QListWidget):
     def __init__(self):
         super().__init__()
 
-        
+        self.row_num = 0
 
     def addData(self, data: str):
-        pass
+        self.insertItem(self.row_num, QListWidgetItem(data))
+        self.row_num += 1
