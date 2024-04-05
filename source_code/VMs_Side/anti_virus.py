07c75150e203fb0d63e8c2f70182611e1ac57cee
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-05 21:12:18 +0300

    Update anti_virus.py

--- source_code/VMs_Side/anti_virus.py
+++ source_code/VMs_Side/anti_virus.py
@@ -14,11 +14,11 @@ class AntiVirus:
 
     @classmethod
     def run(cls):
-        with Protocol.connected_socket(cls.server_ip) as sock:
+        with Protocol.connected_socket(cls.server_ip) as anti_virus:
             print_colored('info', 'Anti Virus has connected to the server')
 
             # Verify connection with the server
-            server_msg = send_and_recv(sock, Messages.ANTI_VIRUS)
+            server_msg = send_and_recv(anti_virus, Messages.ANTI_VIRUS)
             if server_msg != Messages.OK:
                 print_colored('server', server_msg)
                 print_colored('error', 'The server sent a message that is not OK. Exiting')
@@ -30,7 +30,7 @@ class AntiVirus:
                     time.sleep(cls.delay)
                 except KeyboardInterrupt:
                     try:
-                        send_and_recv(sock, Messages.CONNECTION_CLOSED)
+                        send_and_recv(anti_virus, Messages.CONNECTION_CLOSED)
                     except ProtocolError as e:
                         print_colored('error', e)
                         return
@@ -39,33 +39,12 @@ class AntiVirus:
                     return
                 
                 # Perform virus scanning and send results to the server
-                killing_result = cls.find_and_kill_process(cls.virus)
+                virus_proc = FindAndKillProcess(cls.virus)
+                killing_result = virus_proc.kill()
                 if killing_result[0]:   # If a virus was found
                     msg = cls.possibilities[killing_result]
-                    server_msg = send_and_recv(sock, msg)
+                    server_msg = send_and_recv(anti_virus, msg)
                     print_colored('server', server_msg)
-
-    @classmethod
-    def find_and_kill_process(cls, process_name: str) -> Tuple[bool, bool]:
-        """Finds the given process and kills it. Returns a tuple containing two booleans: Found & Killed.
-        
-        Returns (False, False) if the process was not detected,
-        (True, False) if the process was detected, but could not be killed,
-        and (True, True) if it was detected and killed successfully."""
-        
-        ps_output = output_of_command('ps -A')
-        procs = ps_output.split('\n')
-        found_procs = filter_matches(process_name, procs)
-        if len(found_procs) == 0:
-            found = False
-            killed = False
-            print_colored('anti virus' ,'Virus was not detected')
-        else:
-            found = True
-            print_colored('anti virus' ,'Virus detected')
-            pids = find_pids(found_procs)
-            killed = kill_procs(pids)
-        return found, killed
 
 
 if __name__ == '__main__':
