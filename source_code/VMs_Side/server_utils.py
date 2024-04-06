829bc1596081dddfb8de4280245cbddb79735a26
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-06 21:55:39 +0300

    Update server_utils.py

--- source_code/VMs_Side/server_utils.py
+++ source_code/VMs_Side/server_utils.py
@@ -12,19 +12,19 @@ class Sockets:
     def __init__(self):
         """Management of list of sockets"""
 
-        self.sockets: List[SocketID] = []
+        self._sockets: List[SocketID] = []
         self._current_id = 1
 
 
     def add(self, sock: socket) -> int:
         sock_id = self._current_id
-        self.sockets.append(SocketID(sock, sock_id))
+        self._sockets.append(SocketID(sock, sock_id))
         self._current_id += 1
         return sock_id
 
     def _get_index(self, sock_id: int) -> int:
-        for i in range(len(self.sockets)):
-            if self.sockets[i].sock_id == sock_id:
+        for i in range(len(self._sockets)):
+            if self._sockets[i].sock_id == sock_id:
                 return i
         print_colored('error', 'Socket was not found in the list and could not be removed')
         return -1
@@ -33,10 +33,10 @@ class Sockets:
         index = self._get_index(sock_id)
         if index == -1:
             return
-        sock = self.sockets[index].sock
-        self.sockets.pop(index)
+        sock = self._sockets[index].sock
+        self._sockets.pop(index)
         sock.close()
 
     def send_to_all(self, data: str):
-        for sockID in self.sockets:
+        for sockID in self._sockets:
             send(sockID.sock, data)
