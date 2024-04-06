4e15a6c24b47fdcaee692e0bda93cde255e77c8e
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-06 22:03:36 +0300

    Update anti_virus_utils.py

--- source_code/VMs_Side/anti_virus_utils.py
+++ source_code/VMs_Side/anti_virus_utils.py
@@ -13,6 +13,12 @@ class OperatingSystems:
         return platform.system()
 
 
+def pids_name(procs) -> Dict[str, int]:
+    pids_names = {}
+    for line in procs:
+        words = line.split()
+        print(words)
+
 class FindAndKillProcess:
 
     pid_index: Dict[str, int] = {
@@ -44,6 +50,7 @@ class FindAndKillProcess:
         and (True, True) if it was detected and killed successfully."""
         
         all_procs = self._all_procs()
+        pids_name(all_procs)
         found_procs = self._filter_target(all_procs)
         if len(found_procs) == 0:
             found = False
