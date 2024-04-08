f1360c84d940412c138435108b36423ab02fbbf2
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-08 17:07:16 +0300

    Update database.py

--- source_code/VMs_Side/database.py
+++ source_code/VMs_Side/database.py
@@ -1,40 +1,16 @@
 from protocol import *
-from sqlalchemy import create_engine, MetaData, Table, Column, String, LargeBinary
+from hashing import *
+from sqlalchemy import create_engine, MetaData, Table, Column, String
 from sqlalchemy.orm import mapper, sessionmaker
-import scrypt, secrets
-
-
-class ScryptHash:
-
-    SALT_SIZE = 16
-    HASH_SIZE = 32
-
-    def __init__(self, password: str, salt: bytes = None):
-        if salt == None:
-            self.salt = self._generate_salt()
-        else:
-            self.salt = salt
-        self.hash = scrypt.hash(password.encode(), self.salt, 32768, buflen=self.HASH_SIZE)
-    
-    def __new__(cls, password: str, salt: bytes = None) -> bytes:
-        obj = super().__new__(cls)
-        obj.__init__(password, salt)
-        return obj.hash
-    
-    @classmethod
-    def _generate_salt(cls) -> bytes:
-        return secrets.token_bytes(cls.SALT_SIZE)
-    
-    def compare(self, other_password: str) -> bool:
-        other_hash = ScryptHash(other_password, self.salt)
-        return self.hash == other_hash.hash
+from sqlalchemy.exc import IntegrityError
 
 
 class User:
 
     def __init__(self, user: Tuple[str, str]):
-        self.username = user[0]
-        self.password_hash = ScryptHash(user[1])
+        username, password = user
+        self.username = username
+        self.base64_password = ScryptHash(password).create_b64_hash()
 
 
 class Database:
@@ -44,13 +20,13 @@ class Database:
     @classmethod
     def init(cls):
         cls.engine = create_engine(f'sqlite:///{cls.db_file}')
-        
+
         cls.metadata = MetaData()
 
         cls.users_table = Table('users', cls.metadata,
                             Column('username', String, primary_key=True),
-                            Column('password', LargeBinary))
-        
+                            Column('base64_password', String))
+
         mapper(User, cls.users_table)
 
         cls.metadata.create_all(cls.engine)
@@ -63,7 +39,15 @@ class Database:
         cls.session.add(new_user)
         cls.session.commit()
 
+    @classmethod
+    def close(cls):
+        cls.session.close()
+
 
 if __name__ == '__main__':
     Database.init()
-    #Database.add_user(('asaf', 'hi12',))
+    try:
+        Database.add_user(('nadav', 'sex',))
+    except IntegrityError:
+        print_colored('error', 'User already exists')
+    Database.close()
