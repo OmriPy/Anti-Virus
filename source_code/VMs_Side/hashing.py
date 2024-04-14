a3a6b3e26fe871b25eae92fd3c7655b6f89cd44e
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-15 00:13:47 +0300

    Update hashing.py

--- source_code/VMs_Side/hashing.py
+++ source_code/VMs_Side/hashing.py
@@ -3,23 +3,23 @@ from typing import Optional
 
 class ScryptHash:
 
-    SALT_SIZE = 16
-    HASH_SIZE = 32
-    SEP = '|'
+    _SALT_SIZE = 16
+    _HASH_SIZE = 32
+    _SEP = '|'
 
     def __init__(self, plaintext_password: str, salt: Optional[bytes] = None):
         self.salt = salt or self._generate_salt()
-        self.hash = scrypt.hash(plaintext_password.encode(), self.salt, 32768, buflen=self.HASH_SIZE)
+        self.hash = scrypt.hash(plaintext_password.encode(), self.salt, 32768, buflen=self._HASH_SIZE)
 
 
     @classmethod
     def _generate_salt(cls) -> bytes:
-        return secrets.token_bytes(cls.SALT_SIZE)
+        return secrets.token_bytes(cls._SALT_SIZE)
 
     def create_b64_hash(self) -> str:
         b64_salt = base64.b64encode(self.salt).decode()
         b64_hash = base64.b64encode(self.hash).decode()
-        return f'{b64_salt}{self.SEP}{b64_hash}'
+        return f'{b64_salt}{self._SEP}{b64_hash}'
     
     def compare(self, plaintext_password: str) -> bool:
         given_scrypt_hash = ScryptHash(plaintext_password, self.salt)
@@ -27,7 +27,7 @@ class ScryptHash:
 
     @classmethod
     def create_from_b64(cls, b64_password: str) -> 'ScryptHash':
-        b64_salt, b64_scrypt_hash = b64_password.split(cls.SEP, 1)
+        b64_salt, b64_scrypt_hash = b64_password.split(cls._SEP, 1)
         scrypt_salt = base64.b64decode(b64_salt)
         scrypt_hash = base64.b64decode(b64_scrypt_hash)
 
