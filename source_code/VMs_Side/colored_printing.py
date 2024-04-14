3651592eeae8d764dc3da022d0cd9348e03ce71b
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-15 00:12:33 +0300

    Update colored_printing.py

--- source_code/VMs_Side/colored_printing.py
+++ source_code/VMs_Side/colored_printing.py
@@ -1,7 +1,5 @@
-from colorama import init as _init, Fore as _Fore, Style as _Style
 from typing import Tuple, List, Dict, Callable, Optional
-
-#### Colored Printing ####
+from colorama import init as _init, Fore as _Fore, Style as _Style
 
 _init()
 
@@ -24,6 +22,21 @@ def _colorful_str(color: str, prefix: str, msg: str, sock_id: int = 0, username:
         return f"{_Style.BRIGHT}{color}[{prefix}]: {msg}{_Style.RESET_ALL}"
 
 def print_colored(prefix: str, msg: str, lock = None, sock_id: int = 0, username: str = ''):
+    """
+    Prints a message to the console with a specific color based on the prefix provided. 
+    The message can optionally include a socket ID or username for more detailed logging. 
+    If a threading lock is provided, the printing will be thread-safe.
+
+    Parameters:
+    - prefix (str): The category of the message which determines the color of the text.
+    - msg (str): The message to be printed.
+    - lock (optional): A threading lock object to ensure thread-safe printing. Defaults to None.
+    - sock_id (int, optional): The socket ID associated with the message. Defaults to 0, indicating no socket ID.
+    - username (str, optional): The username associated with the message. Defaults to an empty string, indicating no username.
+
+    Returns:
+    - None
+    """
     prefix = prefix.capitalize()
     color = _colors.get(prefix, _Fore.WHITE)
     string = _colorful_str(color, prefix, msg, sock_id, username)
