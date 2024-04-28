235b2151e25792f1478495636b486e4fb64f60af
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-29 00:16:36 +0300

    Update anti_virus.py

--- source_code/VMs_Side/anti_virus.py
+++ source_code/VMs_Side/anti_virus.py
@@ -6,7 +6,7 @@ class AntiVirus:
     server_ip = '127.0.0.1'
     virus = 'lab_rat.py'
     delay = 5
-    possibilities: Dict[Tuple[bool, bool], str] = {
+    scan_outcomes: Dict[Tuple[bool, bool], str] = {
         (False, False): 'No virus detected',
         (True, True): 'The virus was detected and killed successfully',
         (True, False): 'The virus was detected but could not be killed'
@@ -14,47 +14,57 @@ class AntiVirus:
 
     @classmethod
     def run(cls):
-        with Network.connected_socket(cls.server_ip) as anti_virus:
-            print_colored('info', 'Anti Virus has connected to the server')
+        with Network.Client.connected_socket(cls.server_ip) as cls.anti_virus:
+            print_colored(Prefixes.INFO, 'Anti Virus has connected to the server')
 
             # Verify connection with the server
-            server_msg = Network.send_and_recv(anti_virus, Messages.IS_ANTI_VIRUS)
-            if server_msg != Messages.OK:
-                print_colored('server', server_msg)
-                print_colored('error', 'The server sent a message that is not OK. Exiting')
+            try:
+                server_msg = Network.send_and_recv(cls.anti_virus, Messages.IS_ANTI_VIRUS)
+            except ProtocolError as e:
+                print_colored(Prefixes.ERROR, e)
                 return
-            print_colored('info', f'Virus is: {cls.virus}')
+            if server_msg != Messages.OK:
+                print_colored(Prefixes.WARNING, f'Server sent: {server_msg}. Expected: {Messages.OK}. Exiting')
+                return
+            cls.aes = Network.Client.establish_secure_connection(cls.anti_virus)
 
+            print_colored(Prefixes.INFO, f'Virus is: {cls.virus}')
             # Main loop
             virus_proc = FindAndKillProcess(cls.virus)
             while True:
                 # Perform virus scanning and send results to the server
                 try:
-                    killing_result = virus_proc.hunt()
+                    scan_result = virus_proc.hunt()
                 except KeyboardInterrupt:
-                    print_colored('anti virus', 'Scanning was interrupted')
-                    print_colored('info', Messages.CTRL_C)
-                    return
-                if killing_result[0]:   # If a virus was found
-                    msg = cls.possibilities[killing_result]
+                    print_colored(Prefixes.ANTI_VIRUS, 'Scanning was interrupted')
+                    cls.exit()
+                found, killed = scan_result
+                if found:
+                    msg = cls.scan_outcomes[scan_result]
                     try:
-                        server_msg = Network.send_and_recv(anti_virus, msg)
+                        server_msg = Network.send_and_recv(cls.anti_virus, msg, cls.aes)
                     except ProtocolError as e:
-                        print_colored('error', e)
-                        return
-                    print_colored('server', server_msg)
+                        print_colored(Prefixes.ERROR, e)
+                        cls.exit()
+                    print_colored(Prefixes.SERVER, server_msg)
 
                 try:
                     time.sleep(cls.delay)
                 except KeyboardInterrupt:
-                    try:
-                        Network.send_and_recv(anti_virus, Messages.DISCONNECTION)
-                    except ProtocolError as e:
-                        print_colored('error', e)
-                        return
-                    print_colored('anti virus', Messages.DISCONNECTION)
-                    print_colored('info', Messages.CTRL_C)
-                    return
+                    cls.exit()
+
+    @classmethod
+    def exit(cls) -> NoReturn:
+        print_colored(Prefixes.INFO, Messages.CTRL_C)
+        try:
+            server_msg = Network.send_and_recv(cls.anti_virus, Messages.DISCONNECTION, cls.aes)
+        except ProtocolError as e:
+            print_colored(Prefixes.ERROR, e)
+            exit()
+        if server_msg != Messages.OK:
+            print_colored(Prefixes.WARNING, f'Server sent: {server_msg}. Expected: {Messages.OK}')
+        print_colored(Prefixes.ANTI_VIRUS, Messages.DISCONNECTION)
+        exit()
 
 
 if __name__ == '__main__':
