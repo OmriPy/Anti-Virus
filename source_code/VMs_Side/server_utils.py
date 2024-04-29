e7a66c0aa67667966e6eef288f3474b0fc5bd271
Omri <110406612+OmriPy@users.noreply.github.com>
2024-04-30 00:56:48 +0300

    Update server_utils.py

--- source_code/VMs_Side/server_utils.py
+++ source_code/VMs_Side/server_utils.py
@@ -17,21 +17,27 @@ class SocketsList:
         self._current_id = 1
 
 
+    def _get_index(self, sock_id: int) -> int:
+        for i in range(len(self._sockets)):
+            if self._sockets[i].sock_id == sock_id:
+                return i
+        print_colored(Prefixes.ERROR, f'Socket with ID {sock_id} was not found in the list and could not be removed')
+        return -1
+
+    def exists(self, sock_id: int, ret_index: bool = False) -> bool | int:
+        index = self._get_index(sock_id)
+        if ret_index:
+            return index
+        return index != -1
+
     def add(self, sock: socket, aes: AESCipher) -> int:
         sock_id = self._current_id
         self._sockets.append(_Socket(sock, sock_id, aes))
         self._current_id += 1
         return sock_id
 
-    def _get_index(self, sock_id: int) -> int:
-        for i in range(len(self._sockets)):
-            if self._sockets[i].sock_id == sock_id:
-                return i
-        print_colored(Prefixes.ERROR, 'Socket was not found in the list and could not be removed')
-        return -1
-
     def remove(self, sock_id: int, close: bool = True):
-        index = self._get_index(sock_id)
+        index = self.exists(sock_id, True)
         if index == -1:
             return
         sock = self._sockets[index].sock
